e970f914fdfea47c40d3393543ab07b7fc1f6798
adelkhaytai <khaytiadel10@gmail.com>
2023-04-20 14:32:47 +0200

    uspdate

--- src/component/profile/ProfilePhoto.js
+++ src/component/profile/ProfilePhoto.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
+let pict = {
+  border: "black solid 1px",
+};
 const ProfilePhoto = () => {
   return (
-    <div>
-       <img src="/me.jpg" alt="" />
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatem laudantium obcaecati vel quia porro cupiditate quae! Ab exercitationem distinctio quas consequatur, cum consequuntur optio asperiores. Aspernatur, sed! Delectus, aperiam?</p>
+    <div className="con">
+      <img style={pict} src="/me.jpg" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        voluptatem laudantium obcaecati vel quia porro cupiditate quae! Ab
+        exercitationem distinctio quas consequatur, cum consequuntur optio
+        asperiores. Aspernatur, sed! Delectus, aperiam?
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ProfilePhoto
+export default ProfilePhoto;
